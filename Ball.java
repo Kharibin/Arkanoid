eb3791123f7cc3c77a2a3729044199a2a048598a
Kharibin <ai.kharybin@gmail.com>
2017-08-12 20:55:37 +0300

    7 ball fields
    Added speed direction isFrozen fields for Ball class

--- Ball.java
+++ Ball.java
@@ -1,8 +1,47 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed, direction, dx, dy;
+    private boolean isFrozen;
+
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
+    }
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.direction = direction;
+        this.speed = speed;
+        isFrozen = true;
     }
 
     @Override
