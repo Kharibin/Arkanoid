63964c6898c16f701a12eb05716a822cb615ed3d
Kharibin <ai.kharybin@gmail.com>
2017-08-12 18:19:24 +0300

    2
    int height
    int width
    getters setters

--- Arkanoid.java
+++ Arkanoid.java
@@ -1,6 +1,24 @@
 package com.javarush.task.task24.task2413;
 
 public class Arkanoid {
+    private int height, width;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     public static void main(String[] args) {
 
     }
