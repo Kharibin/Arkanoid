dceaa72460a37c2b3af3d574dc2f083644a3f6fa
Kharibin <ai.kharybin@gmail.com>
2017-08-12 18:30:01 +0300

    3
    Ball, Stand, Bricks fields added

--- Arkanoid.java
+++ Arkanoid.java
@@ -1,7 +1,12 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
     private int height, width;
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
 
     public int getHeight() {
         return height;
@@ -16,6 +21,35 @@ public class Arkanoid {
     }
 
     public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
+    public Arkanoid(int width, int height) {
+        this.height = height;
         this.width = width;
     }
 
